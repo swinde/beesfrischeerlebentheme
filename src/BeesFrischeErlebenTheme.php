d7e6f7977d4774ed06b03b53eadc8068e0ec526b
Steffen Winde <inserv@winde-ganzig.de>
2022-12-28 14:15:03 +0100

    +

---
+++ src/BeesFrischeErlebenTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace BeesFrischeErlebenTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class BeesFrischeErlebenTheme extends Plugin implements ThemeInterface
+{
+}
